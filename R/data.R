d4013b6a1bfb1fcadba50e7776d45d002526641d
falk-env <dimitri.falk1@gmail.com>
2022-10-20 17:26:39 +0200

    fix(data): changes in pre-processing

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
-#' Polygon representation of municipalities in Germany (VG250 31.12.)
+#' Polygon representation of municipalities in Germany (VG250 31.12.2021)
 #'
 #' A subset of data from the VG250_GEM product provided by the Federal Agency for Cartography and Geodesy, Germany
 #'
-#' @format Simple feature collection with 10,993 features and 1 field:
+#' @format Simple feature collection of type MULTIPOLYGON with 10,994 features and 1 field:
 #' \describe{
 #'   \item{GEN}{municipality name}
 #'   \item{geometry}{centroid coordinates}
@@ -11,5 +11,5 @@
 #' @note: License: Data licence Germany – attribution – version 2.0
 #' @note: Copyright: GeoBasis-DE / BKG 2022 (modified)
 #' @details: pk <- sf::st_read("VG250_PK.shp")
-#' @details: vg250_gem <- sf::st_read("VG250_GEM.shp") |> sf::st_intersection(pk) |> dplyr::select("GEN") |> sf::st_transform("epsg:4326")
+#' @details: vg250_gem <- sf::st_read("VG250_GEM.shp") |> sf::st_filter(x = _, y = pk) |> dplyr::select("GEN") |> sf::st_transform("epsg:4326")
 "vg250_gem"
