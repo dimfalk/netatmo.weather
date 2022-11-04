9becaefcaf071c1537ae128c3550b40f04208cba
falk-env <dimitri.falk1@gmail.com>
2022-11-04 10:34:55 +0100

    refactor(utils): outsource functions

---
+++ R/get_period.R
@@ -0,0 +1,76 @@
+#' Construct a vector of length 2 and integer type representing UNIX time
+#'
+#' @param x NULL, or "recent", or a vector of length 2 containing from/to timestamps as characters.
+#' @param res numeric. Measurement resolution in minutes.
+#'
+#' @return numeric. Vector of length 2 containing from/to timestamps as UNIX time.
+#' @export
+#'
+#' @examples
+#' p1 <- get_period()
+#' p2 <- get_period(res = 60)
+#' p3 <- get_period(x = "recent")
+#' p4 <- get_period(x = c("2022-06-01", "2022-06-04"))
+get_period <- function(x = NULL,
+                       res = 5) {
+
+  # debugging ------------------------------------------------------------------
+
+  # x <- NULL
+  # x <- "recent"
+  # x <- c("2022-06-01", "2022-06-04")
+  # res <- 5
+
+  # input validation -----------------------------------------------------------
+
+  allowed_p <- c("recent")
+
+  checkmate::assert(
+
+    checkmate::testNull(x),
+    checkmate::test_choice(x, allowed_p),
+    checkmate::test_character(x, len = 2, n.chars = 10)
+  )
+
+  checkmate::assert_numeric(res, len = 1)
+
+  allowed_res <- c(5, 30, 60, 180, 360, 1440)
+  checkmate::assert_choice(res, allowed_res)
+
+  # main -----------------------------------------------------------------------
+
+  now <- lubridate::now()
+
+  # default: in case no input is defined by the user
+  if (is.null(x)) {
+
+    to <-  now |> lubridate::floor_date(unit = "hour")
+    from <- to - 60 * res * 1024
+
+    # query the last 24 hours only
+  } else if (inherits(x, "character") && length(x) == 1 && x == "recent") {
+
+    to <-  now |> lubridate::floor_date(unit = "hour")
+    from <- to - 60 * 60 * 24
+
+    # in case a vector of timestamps is provided c("YYYY-MM-DD", "YYYY-MM-DD")
+  } else if (inherits(x, "character") && all.equal(nchar(x), c(10, 10))) {
+
+    to <- x[2] |> strptime(format = "%Y-%m-%d") |> as.POSIXct()
+    from <- x[1] |> strptime(format = "%Y-%m-%d") |> as.POSIXct()
+
+    #
+    timediff_min <- (as.integer(to) - as.integer(from)) / 60
+    n_queried <- timediff_min / res
+
+    # throw warning if limit is exceeded
+    if (n_queried > 1024) {
+
+      paste0("Based on the defined period '", from, "/", to, "' and the chosen resolution '", res, " min',
+             you are trying to access ", n_queried, " values. Allowed maximum is 1024. The result may be incomplete.") |> warning()
+    }
+  }
+
+  # return object
+  c(from, to) |> as.integer()
+}
---
+++ R/set_device.R
@@ -0,0 +1,63 @@
+#' Manually construct an sf object with user-provided MAC addresses
+#'
+#' @param base_station character. MAC address of Netatmo base station.
+#' @param NAModule1 character. MAC address of Netatmo outdoor module.
+#' @param NAModule2 character. MAC address of Netatmo wind module.
+#' @param NAModule3 character. MAC address of Netatmo precipitation module.
+#' @param lat (optional) numeric. Geodetic latitude in EPSG: 4326.
+#' @param lon (optional) numeric. Geodetic longitude in EPSG: 4326.
+#'
+#' @return Sf object containing relevant MAC addresses for `get_measure()` queries.
+#' @export
+#'
+#' @examples
+#' set_device("70:ee:50:13:54:bc")
+#' set_device("70:ee:50:13:54:bc", "02:00:00:13:57:c8", "06:00:00:02:5f:54", "05:00:00:01:48:96")
+set_device <- function(base_station = NULL,
+                       NAModule1 = NULL,
+                       NAModule2 = NULL,
+                       NAModule3 = NULL,
+                       lat = NULL,
+                       lon = NULL) {
+
+  # debugging ------------------------------------------------------------------
+
+  # base_station <- "70:ee:50:13:54:bc"
+  # NAModule1 <- "02:00:00:13:57:c8"
+  # NAModule2 <- "06:00:00:02:5f:54"
+  # NAModule3 <- "05:00:00:01:48:96"
+  # lat <- 51.44983
+  # lon <- 7.069292
+
+  # input validation -----------------------------------------------------------
+
+  checkmate::assert_character(base_station, n.chars = 17, pattern = "([a-z0-9]{2}:){5}")
+
+  # main -----------------------------------------------------------------------
+
+  n_modules <- paste(base_station, NAModule1, NAModule2, NAModule3, sep = " ") |>
+    stringr::str_trim("right") |>
+    stringr::str_split(" ") |>
+    unlist() |>
+    length()
+
+  temp <- data.frame(status = "man",
+                     time_server = Sys.time(),
+                     base_station = base_station,
+                     timezone = Sys.timezone(),
+                     country = NA,
+                     altitude = NA,
+                     city = NA,
+                     street = NA,
+                     mark = NA,
+                     n_modules = n_modules,
+                     NAModule1 = ifelse(is.null(NAModule1), NA, NAModule1),
+                     NAModule2 = ifelse(is.null(NAModule2), NA, NAModule2),
+                     NAModule3 = ifelse(is.null(NAModule3), NA, NAModule3),
+                     lat = ifelse(is.null(lat), 0, lat),
+                     lon = ifelse(is.null(lon), 0, lon))
+
+  tibble::as_tibble(temp) |> sf::st_as_sf(coords = c("lat", "lon"),
+                                          crs = "epsg:4326",
+                                          agr = "identity")
+}
